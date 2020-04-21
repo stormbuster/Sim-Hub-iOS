df84c68bbddd551ad03027a5cbbafc1c4c2a564b
Stormbuster <robert@welivelikethis.net>
2020-04-21 15:34:26 +0200

    Updated icon

--- Sim Hub/AppDelegate.m
+++ Sim Hub/AppDelegate.m
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.m
-//  Sim Hub
+//  SImHub
 //
-//  Created by Robert Kortenoeven on 21.04.20.
+//  Created by Robert Kortenoeven on 19.04.20.
 //  Copyright © 2020 Robert Kortenoeven. All rights reserved.
 //
 
@@ -17,29 +17,45 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self loadSettings];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
+- (void)loadSettings {
+    NSUserDefaults * standardUserDefaults = [NSUserDefaults standardUserDefaults];
+    NSString * server_preference = [standardUserDefaults objectForKey:@"home_url"];
+    //    bool *homeButtonRight = [defaults boolForKey:@"home_button_right"]; //NSNumber needed here
+
+    if (!server_preference) {
+        [self registerDefaultsFromSettingsBundle];
+    }
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
+#pragma NSUserDefaults
+- (void)registerDefaultsFromSettingsBundle {
+    // this function writes default settings as settings
+    NSString *settingsBundle = [[NSBundle mainBundle] pathForResource:@"Settings" ofType:@"bundle"];
+    if(!settingsBundle) {
+        NSLog(@"Could not find Settings.bundle");
+        return;
+    }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
+    NSDictionary *settings = [NSDictionary dictionaryWithContentsOfFile:[settingsBundle stringByAppendingPathComponent:@"Root.plist"]];
+    NSArray *preferences = [settings objectForKey:@"PreferenceSpecifiers"];
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSMutableDictionary *defaultsToRegister = [[NSMutableDictionary alloc] initWithCapacity:[preferences count]];
+    for(NSDictionary *prefSpecification in preferences) {
+        NSString *key = [prefSpecification objectForKey:@"Key"];
+        if(key) {
+            [defaultsToRegister setObject:[prefSpecification objectForKey:@"DefaultValue"] forKey:key];
+            NSLog(@"writing as default %@ to the key %@",[prefSpecification objectForKey:@"DefaultValue"],key);
+        }
+    }
+
+    [[NSUserDefaults standardUserDefaults] registerDefaults:defaultsToRegister];
+
 }
 
 @end
--- Sim Hub/ViewController.m
+++ Sim Hub/ViewController.m
@@ -1,14 +1,16 @@
 //
 //  ViewController.m
-//  Sim Hub
+//  SImHub
 //
-//  Created by Robert Kortenoeven on 21.04.20.
+//  Created by Robert Kortenoeven on 19.04.20.
 //  Copyright © 2020 Robert Kortenoeven. All rights reserved.
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -16,12 +18,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshWebView) name:UIApplicationWillEnterForegroundNotification object:nil];
+    [self loadWebContent];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)loadWebContent {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *url = [defaults objectForKey:@"home_url"];
+    
+    NSURL *theURL = [[NSURL alloc] initWithString:url];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:theURL]];
 }
+
+- (void) refreshWebView {
+//    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+//    bool *homeButtonRight = [defaults boolForKey:@"home_button_right"];
+//    NSNumber *value = [NSNumber numberWithInt:homeButtonRight ? UIInterfaceOrientationLandscapeLeft : UIInterfaceOrientationLandscapeRight];
+//    [[UIDevice currentDevice] setValue:value forKey:@"orientation"];
+    
+    [self loadWebContent];
+}
+
 
 @end
