9d5f82bd84730b21a148d6332dfe339292577356
Robert Kortenoeven <robert@welivelikethis.net>
2021-08-03 20:14:17 +0200

    Added Pod for masonry

--- Sim Hub/ViewController.m
+++ Sim Hub/ViewController.m
@@ -9,8 +9,11 @@
 #import "ViewController.h"
 #import <WebKit/WebKit.h>
 
-@interface ViewController () <UIWebViewDelegate>
-@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+static NSString *const simhubUrl = @"http://192.168.2.227:8888/Dash#Zen%20Dash";
+
+@interface ViewController () <WKUIDelegate, WKNavigationDelegate>
+@property (nonatomic, strong) WKWebView *webView;
 
 @end
 
@@ -18,37 +21,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-   
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshWebView) name:UIApplicationWillEnterForegroundNotification object:nil];
     
-    [self.webView setDelegate:self];
-    [self.webView setScalesPageToFit:YES];
-    
-    [self loadWebContent];
+    [self setupWebView];
 }
+ 
+- (void)setupWebView {
+    
+    NSString *jScript = @"var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width'); document.getElementsByTagName('head')[0].appendChild(meta);document.body.style.zoom = 1.0;";
 
-- (void)loadWebContent {
+    WKUserScript *wkUScript = [[WKUserScript alloc] initWithSource:jScript injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
+    WKUserContentController *wkUController = [[WKUserContentController alloc] init];
+    [wkUController addUserScript:wkUScript];
+
+    WKWebViewConfiguration *wkWebConfig = [[WKWebViewConfiguration alloc] init];
+    wkWebConfig.userContentController = wkUController;
+
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:wkWebConfig];
+    self.webView.UIDelegate = self;
+    self.webView.navigationDelegate = self;
+    
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSString *url = [defaults objectForKey:@"home_url"];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
     
-    NSURL *theURL = [[NSURL alloc] initWithString:url];
-    [self.webView loadRequest:[NSURLRequest requestWithURL:theURL]];
-}
-
-- (void) refreshWebView {
-//    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-//    bool *homeButtonRight = [defaults boolForKey:@"home_button_right"];
-//    NSNumber *value = [NSNumber numberWithInt:homeButtonRight ? UIInterfaceOrientationLandscapeLeft : UIInterfaceOrientationLandscapeRight];
-//    [[UIDevice currentDevice] setValue:value forKey:@"orientation"];
-    
-    [self loadWebContent];
+    [self.view addSubview:self.webView];
 }
 
 
-#pragma mark UIWebViewDelegate
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
-{
-    NSLog(@"Error : %@",error);
+#pragma mark WebViewDelegate
+
+- (void)webView:(WKWebView *)webView didCommitNavigation:(null_unspecified WKNavigation *)navigation {
+    NSLog(@"finished nav...");
+    [[NSUserDefaults standardUserDefaults] setValue:self.webView.URL.absoluteString forKey:@"lastPageViewed"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+    NSLog(@"finished nav for webView: %@", webView);
+}
+
 
 @end
